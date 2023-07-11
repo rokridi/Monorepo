e2e6085c91a00ce0d46172335b75468f4367e4e0
rokridi <mohamed.aymen.landolsi@gmail.com>
2023-07-11 22:59:15 +0200

    Update AppDelegate.swift

--- DefinitionsKit/demo/AppDelegate.swift
+++ DefinitionsKit/demo/AppDelegate.swift
@@ -10,7 +10,7 @@ import Foundation
 import DefinitionsKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(
